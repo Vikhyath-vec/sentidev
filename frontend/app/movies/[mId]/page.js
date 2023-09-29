409d3fc88803b8eb91237b01e60b5116515f0a2f
Steve-04 <vssteve004@gmail.com>
2023-09-29 11:22:57 +0530

    add review

--- frontend/app/movies/[mId]/page.js
+++ frontend/app/movies/[mId]/page.js
@@ -4,28 +4,6 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// const movieData = {
-//   id: 1,
-//   title: "Interstellar",
-//   description: "Space Adventure!",
-//   image: "/mv_1.jpg",
-//   ratings: 4,
-//   reviews: [
-//     {
-//       id: 1,
-//       user: "Steve",
-//       comment: "Great movie! Highly recommended.",
-//       rating: 4,
-//     },
-//     {
-//       id: 2,
-//       user: "Stephen",
-//       comment: "Enjoyed every minute of it.",
-//       rating: 4,
-//     },
-//   ],
-// };
-
 const recommendedMovies = [
   {
     id: 2,
@@ -53,6 +31,12 @@ export default function MovieDetails({ params }) {
     2: 0,
     1: 0,
   });
+  const [reviewData, setReviewData] = useState({
+    mid: params.mId,
+    title: "",
+    review: "",
+    rating: 0,
+  });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -76,39 +60,34 @@ export default function MovieDetails({ params }) {
     return <div>Loading...</div>
   }
 
-  // const source = "iVBORw0KGgoAAAANSUhEUgAAAFgAAABSCAYAAADQDhNSAAAABHNCSVQICAgIfAhkiAAAFN5JREFUeJztnHl0FFW+xz/VS3rLTkJ2EkICIWEzgICIw8Ao6KCo4zDKuM04bqjPJyLqoAj6VBREHcVtBnXUcUMU3BVUhFFQQJEQkwhJyJ6Qfe10ernzRzVFd9JJukOKd857+Z6Tc6qr7vKrb27d+t3f73tLSk1NFQxBNWj+tw34v44hglXGEMEqY4hglTFEsMoYIlhlDBGsMoYIVhlDBKuMIYJVhu6UdxgaTsSkGZjiRoBGg62umtZfDtFRcliV/szJaYSMHo8hKhZcLqxVpTQe2I2jpUmV/rrjlBGsMZpJ/fPtxJ27CI0+qMd1a3U5NdvepfLDN7A3N5xUX/rwSOJ/exkxZ1+MKTaxx3WXvYuqT96m6MXHcHV2nFRf/UE6FcEeXXAoEx95heBRY/st6+y0UrHlFUrfeg6nNbCb15rMjPjDDSRceCVao6nf8m2Fefx011U4WpsD6icQnBKCx61+jmHTfg2AEIKW3P005exFOJ2YEpKJmDidoMhorzq2ump+eeo+Gr7b4VcfkdNmM/qW1fJU4IYQAntjHY0/7cFaUYKk1RI+fiphWZNBkgCo/24Hh+67fnBu1AdUJzhy6q8Y/8ALAAiXk/x1d3Hsy/e7WaEhcsoskhZdR/j4KcppIQRVH79F4fMP4eqy+Wxfozcw6oa/EnfeH5DcpAkhaD60n7K3X6Bh3y4QLq86w+dcQMayNUgaLQA5K6+j4fuvB+uWvaCNiIhYpUrLbqQtuRdTfDIIQfm7L1O++UUfpQTWyhJqtr1LW2EeoZmnobOEIEkSIaPHETnlLBr27cTZ0eZVyxAdx4SHXiRq+hwkSUIIga22ioLH7qL4xXVYK0uAnuOnvbgArclCWGY2APqQ8J7/9EGCqm6a1hxM+KQZALicTsre+Ue/dep3f8G+6xdQ/fm7IGRyQtKzyH5yE8Hp45RywenjyH5yEyHpWYA8amu2vce+6xdQv/uLfvspe2cjLocDgPBJM9CagwO+P3+gKsGhYyag0cmOSkv+AexN9X7Vc1rbKVh/N/nr71amhqDIaCY9+grhp51B+GlnMOnRV5R529llo2D93RSsvxuntd2vPuxN9bTkHwBAo9MROmZCoLfnF1R108wjRinHbYdzA65fs+09OsqKGbfqGYLCh6E1WRi/+jkANEEGALqa6sldtUQhKxC0HT5E+Lgpiq2NP34bcBv9QdURHBQ5XDnuPFY5oDZa8w9wYOlldFaXAzKxx8ntrC7nwNLLBkSubFOVT1sHE+rOwSazctz9BRUIrJUlFL20vsf5opfWu19kA4OnTZ62DibUjUW43SZAeWENBObkdEbfsqrH+dG3rMKcnD7gdr1s8rR1EKEqwZ6+q9Y4sBESFBHF+AdeQBccCoCtoRZbQy0grxDHP/AC+oioAbXtOWp787NPFqoS7LkE1YdFBFxf0geRtXIDxuHxcnvtbeSs+As5K/6Co11+vI3D4xm3cgOSj/hGf9CHnrBJreWyqgTb6muUY0N0bB8lfSP9ppWEjp0EgHA6+PnBW2kvzqe9OJ+fH7wV4ZT92NCxk0i/6b6A2/e0ydPWwYSqBB9/8wPyai4AxM67hLj5vwfkRUTh82to/OHfyvXGH/5N4QtrlN9x8y8hdt4lAfVhik9R2ve0dTChKsEdZYXKsTnF/5eROSWdtCX3Au4V2vYtVLz/ao9yFVtfpXrbe8rvtCX3BthPmk9bBxOqLjTsTQ3YGmoxREajDw7DGJtEZ3VZr+X1YZEYomLIuGMtWoNRPuly4WhvYdT1f0XS6ZE08pgQLhfCYcfR3opwOpG0WrQGI5l3PU7+2juw1dX0GVc2xiahDw4DoKuxDnvTycWge4PqAffWX3IwTJ8DyHNl57EKzEmjCB41FktKOuakUZgSkjHGJKA19IzhSlotiRde5Xd/lpR0Jm/YAoDTZqWzpgJrRQkdZYW0Hz1MW2EeHWWFytx+3Ea1oHq4MmnRtaT+eRkgu1g6k0U1p95fOK0dOKztGNyxjKKN6yjb9HdV+lKFYI3RxLDpc4ieeQ4Rk89E10+kSgihxHKPo6Ugh5bc/TjaW3F2duDqsuGyd52I7UoaNPogNEEGtEYzOksIoVmTCR0zvs92fcHR0Ubj/n9T+83n1O/5ElenNfCb7gWDSnDI6PHEL7iM6Fnz0ZosPsscf2O3HcmlrSifjtIjdJQfJeH8xcQvWAyAvbmRvdedF3BuTh8WydQXPlZ87soP36Dig39hTkzBPCKd4NQxBKdlYYxN7JV4p7Wd2l2fUvnhG4MydQwKwRHZM0levISwcVN6XBNC4LJ1Kjmyo6/8jZLXN3iVsaRmMPmpzUhaHUII8tcuH3AAfPicCxi7fK3ct9PB/lt+R3tRvleZ5MU3kXLlfwFyDlBrMPpcKjcf2kfJ68/Q+MM3A7IFTjKjYUpMJfOux0i5/BZltQUyqW2FeVS8+xKHn15N6+EcomfNB0BjMFL96SavdrLufQpjTAIAjft2UfziuoGaRHtxAaFjJmJKSEbSaLCMHEP1Z5u9yqRecweGqFj5n7luOUUvPkZXXTW60AhlXgZ5lRgzdyFhmZNpKcjB0dIYsD0DHsGJF1/NyKuXKqFDAKetk5ovtlL54eteo0ZrsjDjzW/RGowIIfj+T2cr7prniHPaOtl3/W9P2uk3xiYy5fmPFFcv79E7lCfCGJvE6S9tQ5IknLZOdl96hleQ3pKaQfyCxcTMXXjCVUSOVRS/vJ7yd18OyJaAR7Ck0zN2+VqSfncNklb28lz2Liref5Wf/+dWar/+CHtjnVcd4bDLbllyGpIk4WhtpjnnezQGI+PuewadJRghBKVvPkf9t9t9G2qyEDVjLtGz5hE+4XSCwodhq61COOw9yjraWpC0OsInTgMgdPQEKj9+E+F0kLDwSiLc5+t3b+8xFdkb62j47iuqPn0HSaslOC0TSatF0uqInDwLc+JI6vd8BS5Xj3592h0QwRoNWfc8pTzucvZ2Hzn3XMuxrz7sU8ThsncxfPYCAAwxCVRsfZWk319L1Bm/AeQ0fd7DS5X4gicSLrqKcaueJWbuQsInTiN84jSizzqX+PMX47J30eoj4N5acJCYuReis4SgswTj6rLRnLufMUsfRh8cihCC4pfWYy0v9m1vZ4fsWez8BEtqhjKFWVJGYxk5htpdn/gVgg2I4JQrbyX+3EWATG7Zpr+Tv+5Ov+amzqoy4s5dhNZkQR8cirXiKCOvvg1NkAEhBEc23E/bkZ5ppfSbV5G8eInXVHQcmiADkVNmERQeRcP3O7yuCacDe3MD0TPPAeTEqe1YFXHz5XiFvbGOw0+v7pHS7w5HaxM1X2xBow8iNDMbSZIwJ6UiabQ0/bSn3/v2m2BT4kgy73oMSaNBCMHRV56k5NW/+R9IFy70oeGKpzHs9F8pC472onyOPHN/jyrDZy8g9c+399t0yOjxWMuP0n70F6/z7Ud/IWrGXIIio9EEGRh2+q+Uaa1i62s0/uindyAETT/uRricREyaDkBY5mkc2/lJvxo3v4M9CQuvUIxr3LeL0jee9beqgsqP30I4nXLH7hEphKD4n4/7/EclL17id9s+ywpB8cuPKz+VPp1OKj9+MxDTASh941nq98oCFUmrI2HhFf3W8ZvgiOyZsnFCUPLGMwEbB2CrqaBuj7dmoTX/J5+qGmNskldWuj+YR4zC6EPo17D3a1ryvOfouj1fYBtgEtZzYB3npC/4TbAxOk45bi0Y+Aqn8oPXvX6XbfItRjEMj/N5vi8YPHzxvvrobkMgaC3IQbifNmO07/484TfBTvf6XJIkdCFhAzQPgtMyvX6HZEz0Wc5l6wyoXXnF6DuG0L2PkLSsgNr2hC4kTFlmO/2QvvpNcFtRnnJ83N0KGBotCRd4z1sJ5y9GHxbZo2h7yeGAEpHC3kV7yZEe5/VhkSScv9jrXPwFl4Nb+Bcohs/+rXLsyUlv8JvgY19+oBwn//EmjDE957v+EDVjDsZuj77WZCFp0XU9yro6rRz7+iO/2z729Uc+o2BJi67rEXgyDo8jasYcv9tW6sUkkvzHmwH5ifHkpDf4TXDNF1tod8v89SFhTHhoY69zXm+IO+9S5bjxwG7lOOH8xT7bKn5pPV3dVoW+0NVYR7EPYYohOk4ZvUIIrz49bfEHhuHxTHhoI3r39NhReoSaL7b0W89vgoXTSd6a2xXVuSkhhewnN/n1JgV59RZx2hkAuBx28h9ZRtPB72UjggyMvPq2HnW6Gmo5uOIaOmurelwDd+iztoqDK66hy62V8IRnrKQ5Zy/5jyzD5V5aR5x2Bgb36qw/RGTPJPvJTZgSUgA5YJ+3ZpnicvaFgFZy9qZ6Wn45SPSZ89Do9GhNZobPuQBjbCKtBT/1KflPWHgFEW4pa/2eL6n+7B06SguJnf97JEnCkpJOw75ddHVLn9sb66j+7B1cXTb04cPQh4aBEHSUFlH5wb/IX3unT5crZMwE0m5coeiG8x6+DWt5MSFpmZiTRiFJEvbWJppz9vZqc1BkNGlLVpJ67Z3o3NOMs9PKofuX0Jrnnx5uQNG04PQssu592itE6ey0Uvnxm1S8909sPkbc1L9/gjkpFSEEufffpGh4M+5cR8yvzwegpeAgP/73or5XhxqNfL2vMpLEaU+8rUhSa776gPxH5LTVsBlzGXef7Md3lBWx99pze1Q3RMeRcNFVxJ93qRLHPi7uzn3g5oCUogNK27cdzmX/jQup3vae4hNqjSaSLv4T017eTtbKDQybPhdJpwfAMnIM5qRUQI50NezdqbRVvHGd4u6EjplA3PxFfXfucvW7PI+bv0gh19nZQfHGE/Hlhr07sbtVPOakVCwjxwBylHDY9LlkrdzAtJe3k3Txn7zIrdm+hf03LgxYhnvSGY3QrMmkXrNMkeN7wt7WQsN3X6EJMigRuOrPN1Ow/q9e5UZcej0jr14q12ltltNFfrzcfEEfESWnjULC5JjJy49T+tbzXmXGLH2I2HN+B0Dtrk9xddmInPZr9G7923EIIWjJ+5Gijetoyd0/IHtOeo+GrbaK6s8203xoP/rQCExxIxRHXBtkIDg1A0vyCYFHV2O9PC+6nPJIEoKW/INEnTmPoLBItAYjxthEand+MiB7MpatUbYVdJQVkb/uTnnUa7SYR4wicsosQjOzFaWRJTmN4NQMtB7ROuFy0bB3J4efXsXRfz7hc8rzF4OeVTbGJBLzmwsZPnsB5qSRfZZ1dXXRUVGMtbwYXXCo4mUA5D92N3XffC5nG/qL2EmSHJCfeTYZt5+QUzX++C2OthZMiSMxJ4xEE9S7QFAIgbW8mGM7PqJm+3t01lT4d8P9QFVdhDklneRLb/Ra/QQK4XLhsllxdtkQdjvCJbtGkkaLpNejDTKgMZgUxc9AcGzHR5S8+SwdRwd/O6+qyp6Oo4ext56Il1Z9uglrZSkh6VkEj8qU0+f9ECNpNGhNll5lAP5AuL2Ozupy2gp/pvVwLqb4EYq40N7apAq5cAqkU6EZbvmp+03cfGifck1jMGFOTMGUkIIxJhHD8DgMUbEMO302klar1OsPnhoH4XRS//0ObHXV2I5V0VlTjrXiKB3lR72CQWHjpigEH7dRDahKsKTTYUkZLf9wuWjt5uK4bFbaCvNoK/QOmiRffgspl9+s1MtZeT0t+QfQ6PUguUe8cOGy2wkdO4nxq59H0mrdsepnKXntqX5taz2cq4gGLSmjkXQ6hKNnPvBkoap81ZQwUiYFeSNLb+HE7ih5fYOyjJa0WjKWrUFnsmBvasDeWCf/NTWgM1nIuH2NMtqbc/b2ELX0BpfNqmyg0ej1mBL6fiEPFCoTfEJ03VFW5H9Fl4u8NUuV+EJQRBRZ921A46FT0BiMZN23gSD3/oyuhlry1iz1O50O0FF+wiZPWwcTqhLsmQXprAlMTNLVUEvug7cqwZmQ9HFkLF8rS5wkiYw71hLi3lrrctjJffBWnwGfvtBZdcImT1sHE6oSrA8fphwHevMALbn7OfL0/YofHD3zHNJuWEHaDSuIPlNOxx9P+Q9kpdXVeMImT1sHE6q+5HTmE66Vo611QG1Uffo2psQUki65BsArkyuEoHzzi1R98vaA2na0n7BJax64G9gX1N2IqD3x/3M5e0qc/EXRxrUc2/Gh1zkhBLVff0zRxrUDbtdTdiVp1RlrqhIs7CduwNd3evxvSNDRTeIkSZL8kjqJHaSee+uEvWvA7fQFdTfBeEiqgsIGOMdJEqOuu4vEi67ucSnl8lvQWULk7VwDINrTJrtKX6FSlWDPgMnxeHAg0BjNZNzxiKIvE0LQuG8XAJFTzwIg8aKrMQxPIH/t8oC/IOVp02AFd7pD1Smi7cjPynHY+KmA/xuuzUmjyH7ibS9ya3d8xKHVSzi0eonXnBw982yyn3gbc5L/SiAkyW2TWzDuYetgQt2NiKVHsNVVA2CIiiFi8pn9V5I0JCy8guynNmNxbyoUQlD61vPkPboM4bAjHHbyHllGyZvPKbEKS0o62U9tlr0Mqf/bisg+E0NUDABd9TV0lPbUVAwGVP8oki40QvmqSHB6JjXbt/oUTQOET5xO5ooniJt3CRp3usnR0U7BuuVUbu2507PpwB46ygqJmDxL3nGk0xM59SyGTZuNtbK018WN1mQh854nCXILXiref42mA/1LUQcC1ffJ6cMiOX3jZ8rnCNoK8yj8xyM05+xDuJyYYpOIyJ5JzNkXeX03RwhBa8FB8h+9o9+Pbpjik8lYvpbQbhKploKD1Gx7j8YfvsFaXYak0RI2fiqj/rJc+Uieo62F76+Zd9JfG+wNp+TDdNGz5jP27se9Yr/HY7S+4sGO9laOvvY0FVtf8T+2oNGQsPBKUi6/GZ0lpMdl4XKBJHmHNl0u8h6+jdpdnwZ+U35C9SkC5LnYWl1O5OQzlUdf6n6zQuC0tlOx9VXyHr6NpgO7A3O9hKA1/wDVn70DkoQlZbSX7929P2enlYIn7qF2h//yrIHglIzg4zBEx5Fw4VVETj1L/vqqJNFVX0PrLznUf7eDum8+C/h7lb1BazITNXMew6bNJmT0eIKGxYAQWKtKadi7k4otvvUbg41TSvD/Rwx9oFllDBGsMoYIVhlDBKuMIYJVxhDBKmOIYJXxH4r7WLwgFoGBAAAAAElFTkSuQmCC";
-
-  const decodeBase64Image = (base64String) => {
-    const binaryString = atob(base64String);
-    const blob = new Blob([binaryString], { type: "image/png" });
-    return URL.createObjectURL(blob);
+  const handleReviewInputChange = (e) => {
+    const { name, value } = e.target;
+    setReviewData({ ...reviewData, [name]: value });
   };
 
-  // const handleReviewChange = (e) => {
-  //   setUserReview(e.target.value);
-  // };
+  const handleReviewSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/movies/add_review`,
+        reviewData
+      );
+
+      console.log("Review submitted successfully:", response.data);
+
+      setReviewData({
+        title: "",
+        review: "",
+        rating: 0,
+      });
+    } catch (error) {
+      console.error("Error submitting review:", error);
+    }
+  };
 
   // const handleRatingChange = (rating) => {
   //   setUserRating(rating);
-  // };
-
-  // const handleAddReview = () => {
-  //   if (userReview.trim() !== "" && userRating > 0) {
-  //     movieData.reviews.push({
-  //       id: movieData.reviews.length + 1,
-  //       user: "Your Name",
-  //       comment: userReview,
-  //       rating: userRating,
-  //     });
-
-  //     setRatingsData((prevData) => ({
-  //       ...prevData,
-  //       [userRating]: prevData[userRating] + 1,
-  //     }));
-
-  //     setUserReview("");
-  //     setUserRating(0);
-  //   }
   // };
 
   // const totalRatings = Object.values(ratingsData).reduce((acc, curr) => acc + curr, 0);
@@ -185,12 +164,18 @@ export default function MovieDetails({ params }) {
 
       {/* Reviews */}
       <div className="mt-8 mr-5 ml-5">
-        <div className="bg-gray-800 rounded-lg p-4 mb-5">
-          <h3 className="text-3xl font-semibold">Overall Review</h3>
+        <div className="bg-gray-800 rounded-lg p-4 mb-4">
+          <h3 className="text-xl font-semibold text-yellow-400">
+            Overall Review
+          </h3>
           <p className="text-lg mt-2">
-            {movieData.summary || "No overall review available."}
+            {movieData.overallReview || "No overall review available."}
           </p>
-          <h3 className="text-3xl font-semibold mt-4">Overall Rating</h3>
+        </div>
+        <div className="bg-gray-800 rounded-lg p-4 mb-4">
+          <h3 className="text-xl font-semibold text-yellow-400">
+            Overall Rating
+          </h3>
           <p className="text-lg mt-2">
             {movieData.overallRating || "No overall rating available."}
           </p>
@@ -253,10 +238,12 @@ export default function MovieDetails({ params }) {
       </div>
 
       <div className="mt-8 ml-5">
-        <h2 className="text-3xl font-semibold mb-4">Average Rating over Time</h2>
+        <h2 className="text-3xl font-semibold mb-4">
+          Average Rating over Time
+        </h2>
         <div className="flex justify-center items-center">
-          <img 
-            src={`data:image/png;base64,${movieData.runningAverage}`} 
+          <img
+            src={`data:image/png;base64,${movieData.runningAverage}`}
             alt="worldcloud"
             className="w-1/2 h-full rounded-lg shadow-lg"
           />
@@ -266,54 +253,72 @@ export default function MovieDetails({ params }) {
       <div className="mt-8 ml-5 mb-5">
         <h2 className="text-3xl font-semibold mb-4">Word Cloud</h2>
         <div className="flex justify-center items-center">
-          <img 
-            src={`data:image/png;base64,${movieData.wordcloud}`} 
+          <img
+            src={`data:image/png;base64,${movieData.wordcloud}`}
             alt="worldcloud"
             className="w-1/2 h-full rounded-lg shadow-lg"
           />
         </div>
       </div>
 
-      {/* Add Your Review */}
-      {/* <div className="container mx-auto py-8 px-4">
-        <h2 className="text-2xl font-extrabold text-white mb-4">
-          Add Your Review
-        </h2>
-        <div className="rounded-lg bg-gray-800 p-4">
-          <textarea
-            className="w-full h-32 px-4 py-2 rounded-lg text-gray-100 bg-gray-700 focus:outline-none focus:ring focus:border-blue-500"
-            placeholder="Write your review here..."
-            value={userReview}
-            onChange={handleReviewChange}
-          />
-          <div className="flex justify-between items-center mt-4">
-            <div className="flex items-center space-x-2">
-              <span className="text-white font-semibold text-lg">
-                Your Rating:
-              </span>
-              <div className="flex space-x-2">
-                {[1, 2, 3, 4, 5].map((rating) => (
-                  <button
-                    key={rating}
-                    className={`${
-                      userRating >= rating ? "text-yellow-400" : "text-gray-400"
-                    } text-2xl focus:outline-none focus:text-yellow-400`}
-                    onClick={() => handleRatingChange(rating)}
-                  >
-                    &#9733;
-                  </button>
-                ))}
-              </div>
-            </div>
+      {/* Review Form */}
+      <div className="mt-8">
+        <h2 className="text-3xl font-semibold">Add a Review</h2>
+        <form onSubmit={handleReviewSubmit}>
+          <div className="mb-4">
+            <label htmlFor="title" className="block text-lg">
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={reviewData.title}
+              onChange={handleReviewInputChange}
+              className="w-full px-3 py-2 text-lg border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="text" className="block text-lg">
+              Review
+            </label>
+            <textarea
+              id="text"
+              name="text"
+              value={reviewData.review}
+              onChange={handleReviewInputChange}
+              rows="4"
+              className="w-full px-3 py-2 text-lg border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+              required
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="rating" className="block text-lg">
+              Rating
+            </label>
+            <input
+              type="number"
+              id="rating"
+              name="rating"
+              value={reviewData.rating}
+              onChange={handleReviewInputChange}
+              min="1"
+              max="5"
+              className="w-16 px-3 py-2 text-lg border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+              required
+            />
+          </div>
+          <div>
             <button
-              onClick={handleAddReview}
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full transition-transform transform hover:scale-105"
+              type="submit"
+              className="bg-blue-700 hover:bg-blue-800 text-white font-semibold py-3 px-6 rounded-full"
             >
               Submit Review
             </button>
           </div>
-        </div>
-      </div> */}
+        </form>
+      </div>
 
       {/* Horizontal Rating Bar */}
       {/* <div className="container mx-auto py-8 px-4">
